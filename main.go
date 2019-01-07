50e114fd0cf655ff782b40dc5ca3a2ddc067ad6a
liaoxx <993501756@qq.com>
2019-01-07 23:43:25 +0800

    orm insert test success

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
-	"github.com/astaxie/beego"
-	_ "untitle_go_project1/models"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql" // import your used driver
 	_ "untitle_go_project1/routers"
 )
 
+// Model Struct
+type User struct {
+	Id   int
+	Name string `orm:"size(100)"`
+}
+
+func init() {
+	// set default database
+	orm.RegisterDataBase("default", "mysql", "root:root@tcp(127.0.0.1:3306)/go_db?charset=utf8", 30)
+
+	// register model
+	orm.RegisterModel(new(User))
+
+	// create table
+	orm.RunSyncdb("default", false, true)
+}
+
 func main() {
-	beego.Run()
+	o := orm.NewOrm()
+	user := User{Name: "slene"}
+
+	// insert
+	o.Insert(&user)
+
 }
