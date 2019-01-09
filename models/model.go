59091329804e9c2a422360de3c1a8d8673eec342
liaoxx <993501756@qq.com>
2019-01-09 22:12:25 +0800

    created controllers 2019-01-09

--- models/model.go
+++ models/model.go
@@ -8,9 +8,8 @@ import (
 func init() {
 	// set default database
 	orm.RegisterDataBase("default", "mysql", "root:root@tcp(127.0.0.1:3306)/go_db?charset=utf8", 30)
-
 	// register model
-	orm.RegisterModel(new(User))
+	//orm.RegisterModel(new(User))
 
 	// create table
 	orm.RunSyncdb("default", false, true)
